50a36741e2d093e65e3da1be2993a5192bd3af89
João Alves <joao.2003.couto@gmail.com>
2024-06-03 12:16:13 +0100

    Removed auth toast

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,14 @@
-import { AuthService } from './../services/auth.service';
+import { AuthService } from '../services/auth.service';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { filter, map, take } from 'rxjs/operators';
-import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private auth: AuthService, private router: Router, private toastController: ToastController) {}
+  constructor(private auth: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot): Observable<boolean | UrlTree> {
     return this.auth.getCurrentUser().pipe(
@@ -19,13 +18,6 @@ export class AuthGuard implements CanActivate {
         if (isAuthenticated) {
           return true;
         } else {
-          this.toastController
-            .create({
-              message: 'Não autorizado',
-              duration: 2000,
-            })
-            .then((toast) => toast.present());
-
           return this.router.createUrlTree(['/welcome']);
         }
       })
