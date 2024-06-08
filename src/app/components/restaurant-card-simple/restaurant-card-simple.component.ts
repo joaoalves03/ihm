b2bde4e458f8b62ec43c202638f3057c827bd240
João Alves <joao.2003.couto@gmail.com>
2024-06-08 01:05:17 +0100

    Images can now be seen in home page

--- src/app/components/restaurant-card-simple/restaurant-card-simple.component.ts
+++ src/app/components/restaurant-card-simple/restaurant-card-simple.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Restaurant} from "../../objects/restaurant"
 import {NavController} from "@ionic/angular"
+import {DataService} from "../../services/data.service"
 
 @Component({
   selector: 'app-restaurant-card-simple',
@@ -10,11 +11,23 @@ import {NavController} from "@ionic/angular"
 export class RestaurantCardSimpleComponent  implements OnInit {
   @Input() restaurant?: Restaurant
 
+  image?: string
+
   constructor(
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private data: DataService
   ) { }
 
-  ngOnInit() {}
+  async ngOnInit() {
+    const images = await this.data.getRestaurantImages(this.restaurant?.id!)
+
+    if(images.length == 0) {
+      this.image = "assets/noimage.png"
+      return
+    }
+
+    this.image = images[Math.floor(Math.random() * images.length)]
+  }
 
   openRestaurantDetails() {
     this.navCtrl.navigateForward(`/restaurant-details/${this.restaurant?.id}`)
--- src/app/components/restaurant-card/restaurant-card.component.ts
+++ src/app/components/restaurant-card/restaurant-card.component.ts
@@ -1,29 +1,44 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Restaurant} from "../../objects/restaurant"
 import {RestaurantIconsComponent} from "../restaurant-icons/restaurant-icons.component"
-import {NavController} from "@ionic/angular"
+import {IonicModule, NavController} from "@ionic/angular"
+import {DataService} from "../../services/data.service"
+import {NgIf} from "@angular/common"
 
 @Component({
   selector: 'app-restaurant-card',
   templateUrl: './restaurant-card.component.html',
   styleUrls: ['./restaurant-card.component.scss'],
   imports: [
-    RestaurantIconsComponent
+    RestaurantIconsComponent,
+    IonicModule,
+    NgIf
   ],
   standalone: true
 })
 export class RestaurantCardComponent  implements OnInit {
   @Input() restaurant?: Restaurant
 
+  image?: string
   distance = Math.floor(Math.random() * 10) + 1
 
   constructor(
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private data: DataService
   ) { }
 
   openRestaurantDetails() {
     this.navCtrl.navigateForward(`/restaurant-details/${this.restaurant?.id}`)
   }
 
-  ngOnInit() {}
+  async ngOnInit() {
+    const images = await this.data.getRestaurantImages(this.restaurant?.id!)
+
+    if(images.length == 0) {
+      this.image = "assets/noimage.png"
+      return
+    }
+
+    this.image = images[Math.floor(Math.random() * images.length)]
+  }
 }
