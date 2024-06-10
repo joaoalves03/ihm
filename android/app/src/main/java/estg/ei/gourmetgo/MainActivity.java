10b6af2f09e7df353eebc497e89831fd9e847f0a
João Alves <joao.2003.couto@gmail.com>
2024-06-10 14:18:51 +0100

    Changed android name and icon

--- android/app/src/main/java/estg/ei/gourmetgo/MainActivity.java
+++ android/app/src/main/java/estg/ei/gourmetgo/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package estg.ei.gourmetgo;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ihm-projeto',
+  appId: 'estg.ei.gourmetgo',
+  appName: 'GourmetGo',
   webDir: 'www',
   server: {
     androidScheme: 'https'
